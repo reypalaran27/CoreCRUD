738dfa5ad47dded04c340171835351dda51cf776
Rey Palaran <reypalaran27@yahoo.com>
2020-04-04 10:17:05 +0800

    Add project files.

---
+++ CoreMVCcrud/Controllers/CustController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreMVCcrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMVCcrud.Controllers
+{
+    public class CustController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CustController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var displaydata = _db.tb_customer.ToList();
+            return View(displaydata);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(NewEmp nec)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(nec);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(nec);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var getempdetails = await _db.tb_customer.FindAsync(id);
+            return View(getempdetails);
+        
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(NewEmp nc)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(nc);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(nc);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var getempdetails = await _db.tb_customer.FindAsync(id);
+            return View(getempdetails);
+
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var getempdetails = await _db.tb_customer.FindAsync(id);
+            return View(getempdetails);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var getempdetails = await _db.tb_customer.FindAsync(id);
+            _db.tb_customer.Remove(getempdetails);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+}
---
+++ CoreMVCcrud/Models/NewEmp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreMVCcrud.Models
+{
+    public class NewEmp
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage ="Enter Customer Name")]
+        [Display(Name = "Customer Name")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Enter Email")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Enter Customer Age")]
+        [Display(Name = "Age")]
+        [Range(20,50)]
+        public int age { get; set; }
+
+        [Required(ErrorMessage = "Enter Customer Gender")]
+        [Display(Name = "Gender")]
+        public string gender { get; set; }
+
+        
+    }
+}
